eccfd741aa0c5f79f5fc8ad11330c23690c58c9c
NoamBarak <57773856+NoamBarak@users.noreply.github.com>
2024-08-07 18:51:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import sys
+import zipfile
+import os
+
+def unzip(zip_file, output_dir):
+    os.makedirs(output_dir, exist_ok=True)     # Create output directory if it doesn't exist
+
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(output_dir)
 
 def main():
     if len(sys.argv) < 2 or len(sys.argv) > 5:
         return
 
+    # Unzip
+    unzip('htr-selex.zip', 'htr-selex')
+    unzip('RNAcompete_intensities.zip', 'RNAcompete_intensities')
+
+
+    # Input files
     rna_filename = "RNAcompete_intensities/" + sys.argv[1]
     htr_filenames = sys.argv[2:]
+    htr_files = {f"htr_filename{i + 1}": "htr-selex/" + htr_filenames[i] for i in range(len(htr_filenames))}
 
-    # Create a dictionary to store HTR-SELEX filenames
-    htr_folder = "htr-selex/"
-    htr_files = {f"htr_filename{i + 1}": htr_folder + htr_filenames[i] for i in range(len(htr_filenames))}
 
-    # # Print contents of each HTR-SELEX file with prefix
-    # for idx, (key, htr_filename) in enumerate(htr_files.items(), start=1):
-    #     print(f"\nContents of HTR-SELEX file {idx} ({key} - {htr_filename}):")
-    #     try:
-    #         with open(htr_filename, 'r') as file:
-    #             print(file.read())
-    #     except FileNotFoundError:
-    #         print(f"Error: File '{htr_filename}' not found.")
 
 
 if __name__ == "__main__":
